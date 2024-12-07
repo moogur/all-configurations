c889e09b9a7e37d7b44444a03ad935f28bdc27b0
Aleksei <a.dvoretskii@platform.live>
2024-12-07 10:42:40 +0300

    [CONF-526] fix query params

--- libs/pinia-fetch-store/src/types/createFetchStore.ts
+++ libs/pinia-fetch-store/src/types/createFetchStore.ts
@@ -9,7 +9,8 @@ export type EndpointDefinition<ResultType, RequestArgument> = {
     url: string;
     convertResponse?: ConvertResponse;
     headers?: RequestInit['headers'];
-    cached?: boolean
+    cached?: boolean;
+    skipErrorNotification?: boolean;
   } & (
     | {
         method: GetMethod;
